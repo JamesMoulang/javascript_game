5fdb4034e16b9b6c3b8e007e560a90b00deb9f44
JamesMoulang <james@jamess-MacBook.local>
2015-02-25 14:44:11 +0000

    Put javascript in separate file

---
+++ script.js
@@ -0,0 +1,20 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0,0,150,75);
+ctx.fillRect(0,0,150,75);
+ctx.font = "30px Times";
+ctx.fillText("Hello World",10,50);
+
+while (true) {
+	update();
+	draw();
+}
+
+function update() {
+
+}
+
+function draw() {
+
+}
