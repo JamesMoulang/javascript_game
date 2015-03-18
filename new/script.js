a46eb0086631c73f57cc61e26216c20dea23ea31
JamesMoulang <james@jamess-MacBook.local>
2015-03-18 14:33:12 +0000

    Fixed input, added collision detection, added css

--- new/script.js
+++ new/script.js
@@ -13,40 +13,56 @@ var directions = {
 	RIGHT: 3
 };
 
-var keycodes = {
+var keycodes1 = {
 	UP: 38,
 	DOWN: 40,
 	LEFT: 37,
 	RIGHT: 39
 };
 
+var keycodes2 = {
+	UP: 87,
+	DOWN: 83,
+	LEFT: 65,
+	RIGHT: 68
+};
+
 var pressed = {};
 var background;
 var players = [];
 
-function Player(x, y, width, height, player1, movespeed, img) {
-	this.init(x, y, width, height, player1, movespeed, img);
+function Player(x, y, width, height, player, movespeed, img) {
+	this.init(x, y, width, height, player, movespeed, img);
 }
 
 Player.prototype = {
-	init: function (x, y, width, height, player1, movespeed, img) {
+	init: function (x, y, width, height, player, movespeed, img) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
 		this.movespeed = movespeed;
-
 		this.direction = directions.NONE;
+		this.player = player;
 
-		mid = new Sprite(img, 40+(player1)*60, 0, 20, 20);
-		up = new Sprite(img, 20+(player1)*60, 0, 20, 20 );
-		left = new Sprite( img, 0+(player1)*60, 20, 20, 20 );
-		down = new Sprite( img, 20+(player1)*60, 40, 20, 20 );
-		right = new Sprite( img, 40+(player1)*60, 20, 20, 20 );
+		// if (player == 1) {
+		// 	mid = new Sprite(img, 40, 0, 20, 20);
+		// 	up = new Sprite(img, 20, 0, 20, 20 );
+		// 	left = new Sprite( img, 0, 20, 20, 20 );
+		// 	down = new Sprite( img, 20, 40, 20, 20 );
+		// 	right = new Sprite( img, 40, 20, 20, 20 );
+		// } else if (player == 2) {
+		// 	mid = new Sprite(img, 100, 0, 20, 20);
+		// 	up = new Sprite(img, 80, 0, 20, 20 );
+		// 	left = new Sprite( img, 60, 20, 20, 20 );
+		// 	down = new Sprite( img, 80, 40, 20, 20 );
+		// 	right = new Sprite( img, 100, 20, 20, 20 );
+		// }
 	},
 
 	draw: function(ctx) {
-		switch(this.direction) {
+		if (this.player == 1) {
+			switch(this.direction) {
 			case directions.NONE:
 				mid.draw( ctx, this.x-this.width/2, this.y-this.height/2);
 				break;
@@ -62,25 +78,80 @@ Player.prototype = {
 			case directions.RIGHT:
 				right.draw( ctx, this.x-this.width/2, this.y-this.height/2);
 				break;
+			}
+		} else if (this.player == 2) {
+			switch(this.direction) {
+			case directions.NONE:
+				mid2.draw( ctx, this.x-this.width/2, this.y-this.height/2);
+				break;
+			case directions.UP:
+				up2.draw( ctx, this.x-this.width/2, this.y-this.height/2);
+				break;
+			case directions.DOWN:
+				down2.draw( ctx, this.x-this.width/2, this.y-this.height/2);
+				break;
+			case directions.LEFT:
+				left2.draw( ctx, this.x-this.width/2, this.y-this.height/2);
+				break;
+			case directions.RIGHT:
+				right2.draw( ctx, this.x-this.width/2, this.y-this.height/2);
+				break;
+			}
 		}
-		
 	},
 
 	update: function(input) {
-		if (input[keycodes.UP]) {
+		var x_move = 0;
+		var y_move = 0;
+		this.direction = directions.NONE;
+
+		if (this.player == 1) {
+			_keycodes = keycodes1;
+		} else if (this.player == 2) {
+			_keycodes = keycodes2;
+		}
+
+		if (input[_keycodes.UP]) {
+			y_move = -1;
 			this.direction = directions.UP;
-			this.y -= this.movespeed;
-		} else if (input[keycodes.DOWN]) {
+		} else if (input[_keycodes.DOWN]) {
+			y_move = 1;
 			this.direction = directions.DOWN;
-			this.y += this.movespeed;
-		} else if (input[keycodes.LEFT]) {
+		}
+
+		if (input[_keycodes.LEFT]) {
+			x_move = -1;
 			this.direction = directions.LEFT;
-			this.x -= this.movespeed;
-		} else if (input[keycodes.RIGHT]) {
+		} else if (input[_keycodes.RIGHT]) {
+			x_move = 1;
 			this.direction = directions.RIGHT;
-			this.x += this.movespeed;
+		}
+
+		var future_x = this.x + x_move * this.movespeed;
+		var future_y = this.y + y_move * this.movespeed;
+
+		if ((future_x - this.width/2) < 0) {
+			this.x = this.width/2;
 		} else {
-			this.direction = directions.NONE;
+			this.x += x_move * this.movespeed;	
+		}
+
+		if ((future_x + this.width/2) > 320) {
+			this.x = 320 - this.width/2;
+		} else {
+			this.x += x_move * this.movespeed;	
+		}
+
+		if ((future_y - this.width/2) < 0) {
+			this.y = this.width/2;
+		} else {
+			this.y += y_move * this.movespeed;	
+		}
+
+		if ((future_y + this.width/2) > 480) {
+			this.y = 480 - this.width/2;
+		} else {
+			this.y += y_move * this.movespeed;	
 		}
 	}
 }
@@ -96,6 +167,11 @@ function main()
 	canvas = document.createElement( "canvas" );
 
 	document.onkeydown=function(e) {
+		e = e || window.event;
+		pressed[e.keyCode] = true;
+	}
+
+	document.onkeypressed=function(e) {
 		e = e || window.event;
 		pressed[e.keyCode] = true;
 	}
@@ -135,7 +211,11 @@ function main()
 	background = new Image();
 	background.src = "resources/background.png";
 	
-	players.push(new Player(10, 10, 20, 20, 0, 10, img));
+	var player1 = new Player(10, 10, 20, 20, 1, 2, img);
+	var player2 = new Player(40, 40, 20, 20, 2, 2, img);
+
+	players.push(player1);
+	players.push(player2);
 }
 
 function run()
@@ -153,7 +233,7 @@ function update()
 	for (var i = 0; i < players.length; i++) {
 		players[i].update(pressed);
 	};
-	pressed = {};
+	// pressed = {};
 }
 
 function render()
--- new/sprite.js
+++ new/sprite.js
@@ -19,9 +19,15 @@ Sprite.prototype.draw = function(ctx, x, y) {
 };
 
 function initSprites(img) {
-	mid = new Sprite(img, 40, 0, 20, 20);
+	mid = new Sprite(img, 40, 40, 20, 20);
 	up = new Sprite(img, 20, 0, 20, 20 );
 	left = new Sprite( img, 0, 20, 20, 20 );
 	down = new Sprite( img, 20, 40, 20, 20 );
 	right = new Sprite( img, 40, 20, 20, 20 );
+
+	mid2 = new Sprite(img, 100, 40, 20, 20);
+	up2 = new Sprite(img, 80, 0, 20, 20 );
+	left2 = new Sprite( img, 60, 20, 20, 20 );
+	down2 = new Sprite( img, 80, 40, 20, 20 );
+	right2 = new Sprite( img, 100, 20, 20, 20 );	
 }
